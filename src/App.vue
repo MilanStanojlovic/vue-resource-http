18c0962266693c5c99cabb86af5ef9af91e5e8d4
Milan <mistikone1@gmail.com>
2020-06-27 13:57:56 +0200

    vue resource

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label for>Username</label>
+          <input type="text" class="form-control" v-model="user.username" />
+        </div>
+        <div class="form-group">
+          <label for>Email</label>
+          <input type="text" class="form-control" v-model="user.email" />
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+        <hr />
+        <button class="btn btn-primary" @click="getData">Get data</button>
+        <hr />
+        <ul class="list-group">
+          <li
+            class="list-group-item"
+            v-for="u in users"
+            :key="u.id"
+          >{{ u.username }} - {{ u.email }}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      },
+      users: []
+    };
+  },
+  methods: {
+    submit() {
+      this.$http.post("", this.user).then(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    },
+
+    getData() {
+      this.$http
+        .get("")
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const resultArray = [];
+          for (const key in data) {
+            resultArray.push(data[key]);
+          }
+          this.users = resultArray;
+        });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource';
+
+
+Vue.use(VueResource);
+Vue.http.options.root = `https://vue-resource-http-175a5.firebaseio.com/data.json`;
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method === 'POST') {
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => { return { messages: response.body } }
+  });
+})
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
